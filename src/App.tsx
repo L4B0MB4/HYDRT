e03ff9924ea99f1f3a5513bb250805ae87df3272
L4B0MB4 <metalfan65@gmail.com>
2025-03-30 15:38:29 +0200

    Adding shadcn ui components

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,20 @@ export const App = () => {
   const fullImageLink = `http://localhost:7251/static/memes/${currentImage}`;
   return (
     <div>
-      <img style={{ width: "80vw" }} alt="" src={fullImageLink} />
+      <div
+        className="absolute inset-0 bg-gradient-to-br from-teal-900/70 via-teal-500/60 to-teal-900/70 backdrop-blur-md z-0"
+        style={{
+          backdropFilter: "blur(12px)",
+          WebkitBackdropFilter: "blur(12px)",
+        }}
+      />
+      <div className="relative flex h-[95vh] w-[95vw] items-center justify-center mx-auto my-4 overflow-hidden rounded-xl">
+        <img
+          src={fullImageLink}
+          alt="Full size display image"
+          className="object-contain relative z-10 w-[90%] h-[90%]"
+        />
+      </div>
     </div>
   );
 };
